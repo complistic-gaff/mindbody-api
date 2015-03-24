14f6b51fca4467ab056bec070cd02f8727da328f
David Bennett <davidbennett@bravevision.com>
2015-03-25 03:10:38 +1030

    Add required to add_clients_to_classes and remove_clients_from_classes.

--- lib/mindbody-api/services/class_service.rb
+++ lib/mindbody-api/services/class_service.rb
@@ -4,11 +4,11 @@ module MindBody
       service "ClassService"
 
       operation :get_classes
-      operation :get_class_visits,           required:[:class_id]
+      operation :get_class_visits,             required: [:class_id]
       operation :get_class_descriptions
       operation :get_class_schedules
-      operation :add_clients_to_classes
-      operation :remove_clients_from_classes
+      operation :add_clients_to_classes,       required: [:client_ids, :class_ids]
+      operation :remove_clients_from_classes,  required: [:client_ids, :class_ids]
     end
   end
 end
